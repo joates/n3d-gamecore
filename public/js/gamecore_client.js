4e8a71a46cb1ff61a12111b0424bfd2d5e3238a9
joates <joates@1022156.no-reply.drupal.org>
2013-08-29 06:32:30 +0100

    changing the lerp formula fixed client smoothing (issue #1 resolved).

--- public/js/gamecore_client.js
+++ public/js/gamecore_client.js
@@ -136,8 +136,15 @@
       game_core.prototype.v_mul_scalar = function(a, b) { return { x:(a.x * b).fixed(), y:(a.y * b).fixed(), z:(a.z * b).fixed() } }
       // For the server, we need to cancel the setTimeout that the polyfill creates
       game_core.prototype.stop_update = function() { window.cancelAnimationFrame(this.updateid) }
+
       // Simple linear interpolation
-      game_core.prototype.lerp = function(p, n, t) { var _t = Number(t); _t = (Math.max(0, Math.min(1, _t))).fixed(); return (p + _t * (n - p)).fixed() }
+      game_core.prototype.lerp = function(p, n, t) {
+        var _t = Number(t);
+        _t = (Math.max(0, Math.min(1, _t))).fixed();
+        //return (p + _t * (n - p)).fixed()
+        return p * (1 - _t) + n * t
+      }
+
       // Simple linear interpolation between 2 vectors
       game_core.prototype.v_lerp = function(v, tv, t) { return { x:this.lerp(v.x, tv.x, t), y:this.lerp(v.y, tv.y, t), z:this.lerp(v.z, tv.z, t) } }
 
